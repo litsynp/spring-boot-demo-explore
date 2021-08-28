58039aab2c256748ce9a6756115ea0393397392c
litsynp <nocte55is@gmail.com>
2021-08-28 14:21:44 +0900

    chore: disable spring security temporarily

--- src/main/java/com/litsynp/demo/DemoApplication.java
+++ src/main/java/com/litsynp/demo/DemoApplication.java
@@ -1,13 +1,16 @@
 package com.litsynp.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+/** Disable default Spring Security */
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
 }
