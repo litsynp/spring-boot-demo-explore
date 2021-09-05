15565c56bb98176415d3ec6c66f4daa440f0f343
litsynp <nocte55is@gmail.com>
2021-09-05 17:55:54 +0900

    feat: add delete user API

--- src/main/java/com/litsynp/demo/domain/user/controller/UserController.java
+++ src/main/java/com/litsynp/demo/domain/user/controller/UserController.java
@@ -7,6 +7,7 @@ import com.litsynp.demo.domain.user.exception.UserNotFoundException;
 import com.litsynp.demo.domain.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,16 +19,16 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 public class UserController {
 
   @Autowired
-  private UserDaoService service;
+  private UserDaoService userService;
 
   @GetMapping("/users")
   public List<User> retrieveAllUsers() {
-    return service.findAll();
+    return userService.findAll();
   }
 
   @GetMapping("/users/{id}")
   public User retrieveUser(@PathVariable int id) {
-    User user = service.findOne(id);
+    User user = userService.findOne(id);
 
     if (user == null) {
       throw new UserNotFoundException("id-" + id);
@@ -38,12 +39,21 @@ public class UserController {
 
   @PostMapping("/users")
   public ResponseEntity<Object> createUser(@RequestBody User user) {
-    User savedUser = service.save(user);
+    User savedUser = userService.save(user);
 
     URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
         .buildAndExpand(savedUser.getId()).toUri();
 
     return ResponseEntity.created(location).build();
+  }
+
+  @DeleteMapping("/users/{id}")
+  public void deleteUser(@PathVariable int id) {
+    User user = userService.deleteById(id);
+
+    if (user == null) {
+      throw new UserNotFoundException("id-" + id);
+    }
   }
 
 }
--- src/main/java/com/litsynp/demo/domain/user/dao/UserDaoService.java
+++ src/main/java/com/litsynp/demo/domain/user/dao/UserDaoService.java
@@ -2,6 +2,7 @@ package com.litsynp.demo.domain.user.dao;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import com.litsynp.demo.domain.user.model.User;
 import org.springframework.stereotype.Component;
@@ -35,6 +36,21 @@ public class UserDaoService {
   public User findOne(int id) {
     for (User user : users) {
       if (user.getId() == id) {
+        return user;
+      }
+    }
+
+    return null;
+  }
+
+  public User deleteById(int id) {
+    Iterator<User> iterator = users.iterator();
+
+    while (iterator.hasNext()) {
+      User user = iterator.next();
+
+      if (user.getId() == id) {
+        iterator.remove();
         return user;
       }
     }
