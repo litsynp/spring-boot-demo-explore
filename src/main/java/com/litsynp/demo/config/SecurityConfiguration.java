f756a94a2b04ed35a1b2316f14061fc3b116b321
litsynp <nocte55is@gmail.com>
2021-09-05 17:16:16 +0900

    fix: disable CSRF

--- src/main/java/com/litsynp/demo/config/SecurityConfiguration.java
+++ src/main/java/com/litsynp/demo/config/SecurityConfiguration.java
@@ -11,6 +11,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/").permitAll();
+    http.authorizeRequests().antMatchers("/").permitAll().and().csrf().disable();
   }
 }
