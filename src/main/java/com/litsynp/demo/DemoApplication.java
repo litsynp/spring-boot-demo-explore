321755e376bf4a47a307ee3f2e330cf85e0ca792
litsynp <nocte55is@gmail.com>
2021-09-05 23:00:02 +0900

    refactor: simplify internationalization code

--- src/main/java/com/litsynp/demo/DemoApplication.java
+++ src/main/java/com/litsynp/demo/DemoApplication.java
@@ -1,15 +1,13 @@
 package com.litsynp.demo;
 
 import java.util.Locale;
-import org.apache.tomcat.util.descriptor.LocalResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 /** Disable default Spring Security */
 @SpringBootApplication(
@@ -22,16 +20,9 @@ public class DemoApplication {
 
   @Bean
   public LocaleResolver localeResolver() {
-    SessionLocaleResolver slr = new SessionLocaleResolver();
-    slr.setDefaultLocale(Locale.US);
-    return slr;
-  }
-
-  @Bean
-  public ResourceBundleMessageSource bundleMessageSource() {
-    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-    messageSource.setBasename("messages");
-    return messageSource;
+    AcceptHeaderLocaleResolver ahlr = new AcceptHeaderLocaleResolver();
+    ahlr.setDefaultLocale(Locale.US);
+    return ahlr;
   }
 
 }
--- src/main/java/com/litsynp/demo/domain/hello/controller/HelloController.java
+++ src/main/java/com/litsynp/demo/domain/hello/controller/HelloController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import com.litsynp.demo.domain.hello.dto.HelloWorldBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -35,9 +36,8 @@ public class HelloController {
   }
 
   @GetMapping(path = "/hello-world-internationalized")
-  public String helloWorldInternationalized(
-      @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-    return messageSource.getMessage("good.morning.message", null, locale);
+  public String helloWorldInternationalized() {
+    return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
   }
 
 }
