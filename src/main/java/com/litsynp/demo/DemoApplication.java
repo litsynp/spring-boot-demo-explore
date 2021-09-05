801af2714582229a3f18598fea7e10600a1e17a3
litsynp <nocte55is@gmail.com>
2021-09-05 22:55:36 +0900

    feat: implement internationalization

--- src/main/java/com/litsynp/demo/DemoApplication.java
+++ src/main/java/com/litsynp/demo/DemoApplication.java
@@ -1,16 +1,37 @@
 package com.litsynp.demo;
 
+import java.util.Locale;
+import org.apache.tomcat.util.descriptor.LocalResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 /** Disable default Spring Security */
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
+@SpringBootApplication(
+    exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class DemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver slr = new SessionLocaleResolver();
+    slr.setDefaultLocale(Locale.US);
+    return slr;
+  }
+
+  @Bean
+  public ResourceBundleMessageSource bundleMessageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasename("messages");
+    return messageSource;
   }
 
 }
--- src/main/java/com/litsynp/demo/domain/hello/controller/HelloController.java
+++ src/main/java/com/litsynp/demo/domain/hello/controller/HelloController.java
@@ -1,14 +1,21 @@
 package com.litsynp.demo.domain.hello.controller;
 
+import java.util.Locale;
 import com.litsynp.demo.domain.hello.dto.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
 public class HelloController {
+
+  @Autowired
+  private MessageSource messageSource;
 
   @GetMapping(path = "/hello")
   public String helloWorld() {
@@ -26,4 +33,11 @@ public class HelloController {
   public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
     return new HelloWorldBean(String.format("Hello World, %s", name));
   }
+
+  @GetMapping(path = "/hello-world-internationalized")
+  public String helloWorldInternationalized(
+      @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+    return messageSource.getMessage("good.morning.message", null, locale);
+  }
+
 }
