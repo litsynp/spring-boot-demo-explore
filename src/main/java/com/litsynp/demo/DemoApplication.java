f71afe1400c292f5203e0a33a2101ffdaca8a359
litsynp <nocte55is@gmail.com>
2021-09-06 23:45:57 +0900

    feat: turn on basic authentication

--- src/main/java/com/litsynp/demo/DemoApplication.java
+++ src/main/java/com/litsynp/demo/DemoApplication.java
@@ -2,16 +2,13 @@ package com.litsynp.demo;
 
 import java.util.Locale;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 /** Disable default Spring Security */
-@SpringBootApplication(
-    exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+@SpringBootApplication
 public class DemoApplication {
 
   public static void main(String[] args) {
