842cc9fb188ad84bdc4fd5ce93913d3c17011065
litsynp <nocte55is@gmail.com>
2021-09-08 16:06:46 +0900

    feat: add user repository

--- src/main/java/com/litsynp/demo/domain/user/repository/UserRepository.java
+++ src/main/java/com/litsynp/demo/domain/user/repository/UserRepository.java
@@ -1,20 +1,9 @@
 package com.litsynp.demo.domain.user.repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import com.litsynp.demo.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Transactional
-public class UserRepository {
-
-  @PersistenceContext
-  private EntityManager entityManager;
-
-  public long insert(User user) {
-    entityManager.persist(user);
-    return user.getId();
-  }
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/litsynp/demo/global/UserDaoServiceCommandLineRunner.java
+++ src/main/java/com/litsynp/demo/global/UserDaoServiceCommandLineRunner.java
@@ -1,25 +1,25 @@
 package com.litsynp.demo.global;
 
 import com.litsynp.demo.domain.user.domain.User;
-import com.litsynp.demo.domain.user.repository.UserRepository;
+import com.litsynp.demo.domain.user.repository.UserDaoService;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserRepositoryCommandLineRunner implements CommandLineRunner {
+public class UserDaoServiceCommandLineRunner implements CommandLineRunner {
 
   private static final Logger log =
-      org.slf4j.LoggerFactory.getLogger(UserRepositoryCommandLineRunner.class);
+      org.slf4j.LoggerFactory.getLogger(UserDaoServiceCommandLineRunner.class);
 
   @Autowired
-  private UserRepository userRepository;
+  private UserDaoService userService;
 
   @Override
   public void run(String... args) throws Exception {
     User user = new User("Jack", "Admin");
-    userRepository.save(user);
+    long insert = userService.insert(user);
     log.info("New user is created : " + user);
   }
 }
