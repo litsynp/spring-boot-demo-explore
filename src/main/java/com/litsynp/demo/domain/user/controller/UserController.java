4d21f4f29b0413873b5afcdffeabe9295f926423
litsynp <nocte55is@gmail.com>
2021-09-09 14:48:10 +0900

    feat: configure for JPA for user API

---
+++ src/main/java/com/litsynp/demo/domain/user/controller/UserController.java
@@ -0,0 +1,74 @@
+package com.litsynp.demo.domain.user.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import javax.validation.Valid;
+import com.litsynp.demo.domain.user.domain.User;
+import com.litsynp.demo.domain.user.exception.UserNotFoundException;
+import com.litsynp.demo.domain.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;;
+
+@RestController
+public class UserController {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @GetMapping("/jpa/users")
+  public List<User> retrieveAllUsers() {
+    return userRepository.findAll();
+  }
+
+  @GetMapping("/jpa/users/{id}")
+  public EntityModel<User> retrieveUser(@PathVariable int id) {
+    Optional<User> user = userRepository.findById(id);
+
+    if (!user.isPresent()) {
+      throw new UserNotFoundException("id-" + id);
+    }
+
+    // "all-users", SERVER_PATH + "/jpa/users"
+    EntityModel<User> resource = EntityModel.of(user.get());
+    WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+    // HATEOAS
+    resource.add(linkTo.withRel("all-users"));
+
+    return resource;
+  }
+
+  @PostMapping("/jpa/users")
+  public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+    User savedUser = userRepository.save(user);
+
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(savedUser.getId()).toUri();
+
+    return ResponseEntity.created(location).build();
+  }
+
+  @DeleteMapping("/jpa/users/{id}")
+  public void deleteUser(@PathVariable int id) {
+    Optional<User> user = userRepository.findById(id);
+    if (!user.isPresent()) {
+      throw new UserNotFoundException("id-" + id);
+    }
+
+    userRepository.delete(user.get());
+  }
+
+}
--- src/main/java/com/litsynp/demo/domain/user/domain/User.java
+++ src/main/java/com/litsynp/demo/domain/user/domain/User.java
@@ -1,44 +1,81 @@
 package com.litsynp.demo.domain.user.domain;
 
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel(description = "All details about the mock user.")
 @Entity
 public class User {
 
   @Id
   @GeneratedValue
-  private long id;
+  private Integer id;
 
-
+  @Size(min = 2, message = "Name should have at least 2 characters")
+  @ApiModelProperty(notes = "Name should have at least 2 characters")
   private String name;
 
-  private String role;
+  @Past
+  @ApiModelProperty(notes = "Birth date should be in the past")
+  private Date birthDate;
 
-  protected User() {}
+  public User() {}
 
-  public User(String name, String role) {
+  public User(Integer id, String name, Date birthDate) {
+    this.id = id;
     this.name = name;
-    this.role = role;
+    this.birthDate = birthDate;
   }
 
-  public long getId() {
+  public Integer getId() {
     return this.id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
   }
 
   public String getName() {
     return this.name;
   }
 
-  public String getRole() {
-    return this.role;
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Date getBirthDate() {
+    return this.birthDate;
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public User id(Integer id) {
+    setId(id);
+    return this;
+  }
+
+  public User name(String name) {
+    setName(name);
+    return this;
+  }
+
+  public User birthDate(Date birthDate) {
+    setBirthDate(birthDate);
+    return this;
   }
 
   @Override
   public String toString() {
-    return "{" + " id='" + getId() + "'" + ", name='" + getName() + "'" + ", role='" + getRole()
-        + "'" + "}";
+    return "{" + " id='" + getId() + "'" + ", name='" + getName() + "'" + ", birthDate='"
+        + getBirthDate() + "'" + "}";
   }
 
 }
