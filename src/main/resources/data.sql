fb8ea1eb88147a7ae39161ec4fa35d60ce8f2908
litsynp <nocte55is@gmail.com>
2021-09-09 15:48:32 +0900

    feat: add post to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,10 @@
-INSERT INTO USER
-VALUES (1, sysdate(), 'AB');
-INSERT INTO USER
-VALUES (2, sysdate(), 'Jill');
-INSERT INTO USER
-VALUES (3, sysdate(), 'Jam');
+INSERT INTO user (`id`, `birth_date`, `name`)
+VALUES (10001, sysdate(), 'AB');
+INSERT INTO user (`id`, `birth_date`, `name`)
+VALUES (10002, sysdate(), 'Jill');
+INSERT INTO user (`id`, `birth_date`, `name`)
+VALUES (10003, sysdate(), 'Jam');
+INSERT INTO post (`id`, `description`, `user_id`)
+VALUES(11001, 'My First Post', 10001);
+INSERT INTO post (`id`, `description`, `user_id`)
+VALUES(11002, 'My Second Post', 10001);
